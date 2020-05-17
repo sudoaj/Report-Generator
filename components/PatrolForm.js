82caad1f6dbebc5a328dabb3a3acbc7312500452
shalam15 <salamajayi@yahoo.com>
2020-05-17 05:11:18 -0400

    done with report form, minor chnages left

--- components/PatrolForm.js
+++ components/PatrolForm.js
@@ -1,19 +1,172 @@
 
 import React, { Component, useState, useEffect } from 'react';
-import { StyleSheet, TextInput, View, Text,  } from 'react-native';
+import { StyleSheet, TextInput, View, Text, } from 'react-native';
+import { CheckBox } from 'react-native-elements'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 
 export default function PatrolForms(props) {
-    
+    const [Patrol, setPatrol] = useState(false);
+    const [LockedDoors, setLockedDoors] = useState(false);
+    const [UnLockedDoors, setUnLockedDoors] = useState(false);
+    const [MonitoredCamera, setMonitoredCamera] = useState(false);
+    const [SecurityRisk, setSecurityRisk] = useState(false);
+    const [ExtraNotes, setExtraNotes] = useState(false);
+    const [AttachedFile, setAttachedFile] = useState(false);
+
+
+
+
     return (
-        
+        <KeyboardAwareScrollView>
+
+
         <View style={styles.container}>
-          <Text>Hi</Text>    
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Patrol'
+                checked={Patrol}
+                onPress={() => setPatrol(!Patrol)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Locked Doors'
+                checked={LockedDoors}
+                onPress={() => setLockedDoors(!LockedDoors)}
+
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Unlocked Doors'
+                checked={UnLockedDoors}
+                onPress={() => setUnLockedDoors(!UnLockedDoors)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Monitored Camera'
+                checked={MonitoredCamera}
+                onPress={() => setMonitoredCamera(!MonitoredCamera)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='No Security Risk, All CLear for the Hour'
+                checked={SecurityRisk}
+                onPress={() => setSecurityRisk(!SecurityRisk)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Extra Notes'
+                checked={ExtraNotes}
+                onPress={() => setExtraNotes(!ExtraNotes)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
+            <CheckBox
+                checkedColor='blue'
+                uncheckedColor='red'
+                title='Attached Files'
+                checked={AttachedFile}
+                onPress={() => setAttachedFile(!AttachedFile)}
+            />
+            <View style={styles.textAreaContainer} >
+            <TextInput
+               style={styles.textArea}
+               underlineColorAndroid="transparent"
+               placeholder="Type something"
+               placeholderTextColor="grey"
+               numberOfLines={5}
+               multiline={true}
+                />
+                </View>
         </View>
+        </KeyboardAwareScrollView>
+
     );
 }
+
 const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
-       
+
     },
-});
+    textAreaContainer: {
+
+      borderColor: 'lightgrey',
+      borderWidth: 1,
+      padding: 5,
+      marginBottom:15,
+      marginLeft:10,
+      marginRight:10,
+      backgroundColor:'lightgrey'
+
+    },
+    textArea: {
+        color:'#000',
+      height: 50,
+      justifyContent: "flex-start"
+    }
+  })
--- screens/ReportFormScreen.js
+++ screens/ReportFormScreen.js
@@ -1,39 +1,33 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { StyleSheet, View, Button, TouchableHighlight, Text } from 'react-native';
+import { StyleSheet, View, Button, TouchableHighlight, Text, ScrollView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-
-
-import TopTime from '../components/TopTime'
-import TopInputs from '../components/TopInputs'
 import PatrolForms from '../components/PatrolForm'
 
 
 export default function ReportFormScreen() {
   return (
     <View style={styles.container}>
-      <TopTime></TopTime>
-      
-      <TopInputs></TopInputs>
+<Text style={[{fontSize:50}, {textAlign:'center'}]}>1900</Text>
       <PatrolForms></PatrolForms>
     <View>
       <TouchableHighlight
         style={styles.submit1}
         onPress={() => generatReport()}
         underlayColor='#fff'>
-        <Text style={styles.submitText}>Generate Report</Text>
+        <Text style={styles.submitText}>Save As Incomplete </Text>
       </TouchableHighlight>
       </View>
       <View>
       <TouchableHighlight
-        style={styles.submit1}
+        style={styles.submit2}
         onPress={() => generatReport()}
         underlayColor='#fff'>
-        <Text style={styles.submitText}>Generate Report</Text>
+        <Text style={styles.submitText2}>Save Hour</Text>
       </TouchableHighlight>
       </View>
       </View>
-
   );
 }
 const generatReport=()=>{
@@ -53,12 +47,30 @@ const styles = StyleSheet.create({
     marginBottom:5,
     paddingTop: 10,
     paddingBottom: 15,
-    backgroundColor: 'grey',
+    backgroundColor: '#fff200',
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  submit2: {
+    marginRight: 100,
+    marginLeft: 100,
+    marginTop: 5,
+    marginBottom:5,
+    paddingTop: 10,
+    paddingBottom: 15,
+    backgroundColor: 'blue',
     borderRadius: 50,
     borderWidth: 1,
     borderColor: '#fff'
   },
   submitText: {
+    fontSize: 20,
+    color: '#000',
+    textAlign: 'center',
+  }
+  ,
+  submitText2: {
     fontSize: 20,
     color: '#fff',
     textAlign: 'center',
